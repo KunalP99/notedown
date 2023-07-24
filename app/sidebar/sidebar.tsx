000d24439cc9b4c59240f3302e6bf91118ac4291
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-07-24 21:11:56 +0100

    feat: style sidebar

--- app/sidebar/sidebar.tsx
+++ app/sidebar/sidebar.tsx
@@ -1,7 +1,44 @@
+import Image from "next/image"
+import Link from "next/link"
+import styles from './sidebar.module.scss'
+
 const Sidebar = () => {
   return (
-    <div>
-
+    <div className={styles.sidebar}>
+      <div className={styles.logo}>
+        <Image
+          src="/assets/logo.png"
+          width={39}
+          height={39}
+          alt="logo"
+        />
+        <h1>NoteDown</h1>
+      </div>
+      <div className={styles.btnContainer}>
+        <Link href='#' className={styles.newNoteBtn}>
+          <Image
+            src={"/assets/plus.svg"}
+            width={18}
+            height={18}
+            alt="Plus sign"
+          />New Note</Link>
+        <Link href="#">
+          <Image
+            src={'/assets/sidebar/notes-icon.svg'}
+            width={32}
+            height={32}
+            alt="Notes icon"
+          />
+          Notes</Link>
+        <Link href="#">
+          <Image
+            src={'/assets/sidebar/help-icon.svg'}
+            width={32}
+            height={32}
+            alt="Help icon"
+          />
+          Help</Link>
+      </div>
     </div>
   )
 }
