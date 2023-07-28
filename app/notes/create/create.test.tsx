1a819c5ba22d0a65ad4a8b1682b9925105cd6dec
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-07-28 15:59:29 +0100

    test: add tests for create note page

---
+++ app/notes/create/create.test.tsx
@@ -0,0 +1,45 @@
+import { screen, render, within, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import user from '@testing-library/user-event'
+import CreateNote from './page'
+
+beforeEach(() => {
+  render(<CreateNote />)
+})
+
+describe('Create Note', () => {
+  const onSubmit = jest.fn()
+
+  it('renders correctly', () => {
+    const headingElement = screen.getByRole('heading', { name: /create note/i })
+    expect(headingElement).toBeInTheDocument()
+
+    const titleInput = screen.getByRole('textbox', { name: /title/i })
+    expect(titleInput).toBeInTheDocument()
+
+    const noteInput = screen.getByRole('textbox', { name: /note/i })
+    expect(noteInput).toBeInTheDocument()
+
+    const submitBtn = screen.getByRole('button', { name: /create/i })
+    expect(submitBtn).toBeInTheDocument()
+  })
+
+  it('submits form if all fields pass validation', async () => {
+    const titleInput = screen.getByPlaceholderText(/title/i)
+    user.type(titleInput, 'Note Title')
+
+    const noteInput = screen.getByPlaceholderText(/start writing your note here!/i)
+    user.type(noteInput, '# Hello, *world*!')
+
+    const dropdown = screen.getByRole('combobox', { name: /tag/i })
+    user.selectOptions(dropdown, within(dropdown).getByRole('option', { name: /green/i }))
+
+    const submitBtn = screen.getByRole('button', { name: /create/i })
+    user.click(submitBtn)
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalledTimes(1)
+    })
+
+    expect(onSubmit).toHaveBeenCalledWith({ lazy: true })
+  })
+})
