d1aabf2aea91f814249766a9d9f200ee1535ef1b
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-08-07 12:51:24 +0100

    test(create form): update tests and mock user context

--- app/notes/create/create.test.tsx
+++ app/notes/create/create.test.tsx
@@ -2,6 +2,7 @@ import { screen, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import user from '@testing-library/user-event'
 import CreateNoteForm from '@/app/components/forms/CreateNoteForm'
+import { UserContext } from '@/app/components/context/UserContext'
 
 describe('Create Note', () => {
   const setTitle = jest.fn()
@@ -9,23 +10,50 @@ describe('Create Note', () => {
   const setTag = jest.fn()
   const onSubmit = jest.fn()
   const setPreview = jest.fn()
+  const setUser = jest.fn()
+
+  const mockUser = {
+    user: {
+      email: 'exampleemail@gmail.com',
+      given_name: 'Kunal Patel',
+      name: 'Kunal',
+      picture: 'examplepicture',
+      sub: '1234'
+    },
+    setUser
+  }
+
+  const emptyUser = {
+    user: {
+      email: '',
+      given_name: '',
+      name: '',
+      picture: '',
+      sub: ''
+    },
+    setUser
+  }
 
   beforeEach(() => {
     onSubmit.mockClear()
   })
 
-  it('renders correctly', () => {
-    render(<CreateNoteForm
-      title='Note Title'
-      setTitle={setTitle}
-      note='# Hello, *world*!'
-      setNote={setNote}
-      tag='#e2ebe0'
-      setTag={setTag}
-      onSubmit={onSubmit}
-      preview={false}
-      setPreview={setPreview}
-    />)
+  it('renders correctly when user is logged in', () => {
+    render(
+      <UserContext.Provider value={mockUser}>
+        <CreateNoteForm
+          title='Note Title'
+          setTitle={setTitle}
+          note='# Hello, *world*!'
+          setNote={setNote}
+          tag='#e2ebe0'
+          setTag={setTag}
+          onSubmit={onSubmit}
+          preview={false}
+          setPreview={setPreview}
+        />
+      </UserContext.Provider>
+    )
 
     const titleInput = screen.getByPlaceholderText(/title/i)
     expect(titleInput).toBeInTheDocument()
@@ -43,17 +71,21 @@ describe('Create Note', () => {
   it('submits form if all fields pass validation', async () => {
     user.setup()
 
-    render(<CreateNoteForm
-      title='Note Title'
-      setTitle={setTitle}
-      note='# Hello, *world*!'
-      setNote={setNote}
-      tag='#e2ebe0'
-      setTag={setTag}
-      onSubmit={onSubmit}
-      preview={false}
-      setPreview={setPreview}
-    />)
+    render(
+      <UserContext.Provider value={mockUser}>
+        <CreateNoteForm
+          title='Note Title'
+          setTitle={setTitle}
+          note='# Hello, *world*!'
+          setNote={setNote}
+          tag='#e2ebe0'
+          setTag={setTag}
+          onSubmit={onSubmit}
+          preview={false}
+          setPreview={setPreview}
+        />
+      </UserContext.Provider>
+    )
 
     const submitBtn = screen.getByRole('button', { name: /create/i })
     await user.click(submitBtn)
@@ -64,17 +96,21 @@ describe('Create Note', () => {
   it('submits form if only title is filled out', async () => {
     user.setup()
 
-    render(<CreateNoteForm
-      title='Note Title'
-      setTitle={setTitle}
-      note=''
-      setNote={setNote}
-      tag=''
-      setTag={setTag}
-      onSubmit={onSubmit}
-      preview={false}
-      setPreview={setPreview}
-    />)
+    render(
+      <UserContext.Provider value={mockUser}>
+        <CreateNoteForm
+          title='Note Title'
+          setTitle={setTitle}
+          note=''
+          setNote={setNote}
+          tag=''
+          setTag={setTag}
+          onSubmit={onSubmit}
+          preview={false}
+          setPreview={setPreview}
+        />
+      </UserContext.Provider>
+    )
 
     const submitBtn = screen.getByRole('button', { name: /create/i })
     await user.click(submitBtn)
@@ -84,20 +120,45 @@ describe('Create Note', () => {
   it('does not submit form if title is not filled', async () => {
     user.setup()
 
-    render(<CreateNoteForm
-      title=''
-      setTitle={setTitle}
-      note='# Hello, *world*!'
-      setNote={setNote}
-      tag='#e2ebe0'
-      setTag={setTag}
-      onSubmit={onSubmit}
-      preview={false}
-      setPreview={setPreview}
-    />)
+    render(
+      <UserContext.Provider value={mockUser}>
+        <CreateNoteForm
+          title=''
+          setTitle={setTitle}
+          note='# Hello, *world*!'
+          setNote={setNote}
+          tag='#e2ebe0'
+          setTag={setTag}
+          onSubmit={onSubmit}
+          preview={false}
+          setPreview={setPreview}
+        />
+      </UserContext.Provider>
+    )
 
     const submitBtn = screen.getByRole('button', { name: /create/i })
     await user.click(submitBtn)
     expect(onSubmit).not.toHaveBeenCalled()
+  })
+
+  it('does not render create button when not logged in', async () => {
+    render(
+      <UserContext.Provider value={emptyUser}>
+        <CreateNoteForm
+          title=''
+          setTitle={setTitle}
+          note=''
+          setNote={setNote}
+          tag=''
+          setTag={setTag}
+          onSubmit={onSubmit}
+          preview={false}
+          setPreview={setPreview}
+        />
+      </UserContext.Provider>
+    )
+
+    const submitBtn = screen.queryByRole('button', { name: /create/i })
+    expect(submitBtn).toBeNull()
   })
 })
